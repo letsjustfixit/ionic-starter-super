c49f315518c78bcde521afe18ef278a0bfe4a3ae
Max Lynch <max@drifty.com>
2016-11-21 12:12:35 -0600

    Maps

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+
+import { GoogleMap, GoogleMapsLatLng } from 'ionic-native';
 
 declare var google: any;
 
@@ -10,40 +12,41 @@ declare var google: any;
 export class MapPage {
   @ViewChild('map') map;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, public platform: Platform) {}
 
-  ionViewDidLoad() {
-    console.log('Hello MapPage Page');
-
-    let mapEle = this.map.nativeElement; //document.getElementById('map');
-
+  initJSMaps(mapEle) {
     let map = new google.maps.Map(mapEle, {
       center: { lat: 43.071584, lng: -89.380120 },
       zoom: 16
     });
+  }
 
-    /*
-    mapData.forEach(markerData => {
-      let infoWindow = new google.maps.InfoWindow({
-        content: `<h5>${markerData.name}</h5>`
-      });
+  initNativeMaps(mapEle) {
+    this.map = new GoogleMap(mapEle);
+    mapEle.classList.add('show-map');
 
-      let marker = new google.maps.Marker({
-        position: markerData,
-        map: map,
-        title: markerData.name
-      });
-
-      marker.addListener('click', () => {
-        infoWindow.open(map, marker);
-      });
+    GoogleMap.isAvailable().then(() => {
+      const position = new GoogleMapsLatLng(43.074395, -89.381056);
+      this.map.setPosition(position);
     });
-    */
+  }
 
-    google.maps.event.addListenerOnce(map, 'idle', () => {
-      mapEle.classList.add('show-map');
-    });
+  ionViewDidLoad() {
+    let mapEle = this.map.nativeElement;
 
+    if (!mapEle) {
+      console.error('Unable to initialize map, no map element with #map view reference.');
+      return;
+    }
+
+    // Disable this switch if you'd like to only use JS maps, as the APIs
+    // are slightly different between the two. However, this makes it easy
+    // to use native maps while running in Cordova, and JS maps on the web.
+    if (this.platform.is('cordova') === true) {
+      this.initNativeMaps(mapEle);
+    } else {
+      this.initJSMaps(mapEle);
+    }
   }
 
 }
