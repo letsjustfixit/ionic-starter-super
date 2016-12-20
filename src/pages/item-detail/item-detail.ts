83231feae107e5ed931b2b8ae7588e49e857e8d6
Max Lynch <max@drifty.com>
2016-12-20 13:41:25 -0600

    Simpler

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -5,50 +5,13 @@ import { Items } from '../../providers/providers';
 
 @Component({
   selector: 'page-item-detail',
-  templateUrl: 'item-detail.html',
-  host: {
-    '(window:resize)': 'onWindowResize($event)'
-  }
+  templateUrl: 'item-detail.html'
 })
 export class ItemDetailPage {
   item: any;
 
-  headerHeight: number;
-  translateAmt: number;
-  scaleAmt: number;
-
-  @ViewChild('profilePic') profilePic;
-
-
-  constructor(public navCtrl: NavController, navParams: NavParams, public renderer: Renderer, items: Items) {
+  constructor(public navCtrl: NavController, navParams: NavParams, items: Items) {
     this.item = navParams.get('item') || items.defaultItem;
-  }
-
-  onWindowResize(ev) {
-    this.headerHeight = this.profilePic.clientHeight;
-  }
-
-  onScroll(ev: ScrollEvent) {
-    ev.domWrite(() => {
-      console.log('SCROLL', -ev.scrollTop);
-      // DOM writes must go within domWrite()
-      // to prevent any layout thrashing
-      //this.profilePic.nativeElement.style.transform = `translateY(${-ev.scrollTop}px)`;
-
-      if(ev.scrollTop >= 0){
-        this.translateAmt = ev.scrollTop / 2;
-        this.scaleAmt = 1;
-      } else {
-        this.translateAmt = 0;
-        this.scaleAmt = -ev.scrollTop / this.headerHeight + 1;
-      }
-
-      this.renderer.setElementStyle(this.profilePic.nativeElement, 'webkitTransform', 'translate3d(0,' + this.translateAmt + 'px,0) scale('+this.scaleAmt+','+this.scaleAmt+')');
-    });
-  }
-
-  ionViewDidLoad() {
-    console.log('Profile image', this.profilePic)
   }
 
 }
