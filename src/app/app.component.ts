44c40f057951c626a0ec9eaf0895e0b9ab07da6e
Max Lynch <max@drifty.com>
2016-12-20 17:36:19 -0600

    Menu map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { Settings } from '../providers/providers';
 
 import { FirstRunPage } from '../pages/pages';
+import { CardsPage } from '../pages/cards/cards';
+import { ContentPage } from '../pages/content/content';
+import { LoginPage } from '../pages/login/login';
+import { MapPage } from '../pages/map/map';
+import { SignupPage } from '../pages/signup/signup';
+import { TabsPage } from '../pages/tabs/tabs';
+import { TutorialPage } from '../pages/tutorial/tutorial';
+import { WelcomePage } from '../pages/welcome/welcome';
+import { ListMasterPage } from '../pages/list-master/list-master';
+import { ItemCreatePage } from '../pages/item-create/item-create';
+import { ItemDetailPage } from '../pages/item-detail/item-detail';
+import { MenuPage } from '../pages/menu/menu';
+import { SettingsPage } from '../pages/settings/settings';
+import { SearchPage } from '../pages/search/search';
 
 import { TranslateService } from 'ng2-translate/ng2-translate';
 
@@ -30,17 +44,28 @@ import { TranslateService } from 'ng2-translate/ng2-translate';
 export class MyApp {
   rootPage = FirstRunPage;
 
-  pages: any[];
+  @ViewChild(Nav) nav: Nav;
+
+  pages: any[] = [
+    { title: 'First Run', component: FirstRunPage },
+    { title: 'Tutorial', component: TutorialPage },
+    { title: 'Welcome', component: WelcomePage },
+    { title: 'Tabs', component: TabsPage },
+    { title: 'Cards', component: CardsPage },
+    { title: 'Content', component: ContentPage },
+    { title: 'Login', component: LoginPage },
+    { title: 'Signup', component: SignupPage },
+    { title: 'Map', component: MapPage },
+    { title: 'Master Detail', component: ListMasterPage },
+    { title: 'Menu', component: MenuPage },
+    { title: 'Settings', component: SettingsPage },
+    { title: 'Search', component: SearchPage }
+  ]
 
   constructor(translate: TranslateService, platform: Platform, settings: Settings) {
     // Set the default language for translation strings, and the current language.
     translate.setDefaultLang('en');
     translate.use('en')
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'First Run', component: FirstRunPage }
-    ];
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -48,5 +73,11 @@ export class MyApp {
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 }
